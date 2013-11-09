be1e5b74dae1f2866164f60f876a0335b93a1c4c
Connor McCoy <cmccoy@fhcrc.org>
2013-11-08 16:27:05 -0800

    protocol buffer for interchange

--- src/eigen_test.cpp
+++ src/eigen_test.cpp
@@ -1,9 +1,39 @@
+#include <atomic>
 #include <vector>
 #include <iostream>
+#include <boost/math/tools/minima.hpp>
 
 #include "gtr.hpp"
 
 using namespace gtr;
+using boost::math::tools::brent_find_minima;
+
+double star_likelihood(const GTRModel& model,
+                       const std::vector<Sequence>& sequences)
+{
+    double result = 0.0;
+
+#pragma omp parallel for reduction(+:result)
+    for(size_t i = 0; i < sequences.size(); i++) {
+        result += model.logLikelihood(sequences[i]);
+    }
+    return result;
+}
+
+void estimate_branch_lengths(const GTRModel& model,
+                             std::vector<Sequence>& sequences)
+{
+#pragma omp parallel for
+    for(size_t i = 0; i < sequences.size(); i++) {
+        Sequence& s = sequences[i];
+        auto f = [&model, &s](const double d) {
+            s.distance = d;
+            return - model.logLikelihood(s);
+        };
+        boost::uintmax_t max_iter = 50;
+        s.distance = brent_find_minima(f, 1e-7, 1.0, 8, max_iter).second;
+    }
+}
 
 int main()
 {
