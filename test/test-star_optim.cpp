a7199401e11060d74033e3295556f4c3f3953069
Connor McCoy <cmccoy@fhcrc.org>
2013-11-22 08:51:55 -0800

    working tests against bpp

--- test/test-star_optim.cpp
+++ test/test-star_optim.cpp
@@ -8,10 +8,64 @@
 #include <Bpp/Numeric/Prob/ConstantDistribution.h>
 #include <Bpp/Numeric/Prob/GammaDiscreteDistribution.h>
 #include <Bpp/Phyl/Distance/DistanceEstimation.h>
+#include <Bpp/Phyl/Likelihood/DRHomogeneousTreeLikelihood.h>
 #include <Bpp/Phyl/Model.all>
+#include <Bpp/Phyl/TreeTemplate.h>
 #include <Bpp/Seq/Alphabet/DNA.h>
 #include <Bpp/Seq/Container/VectorSiteContainer.h>
 #include <Bpp/Seq/Sequence.h>
+
+bpp::DNA DNA;
+
+bpp::VectorSiteContainer createSites(const Sequence& sequence)
+{
+    std::vector<std::string> names {"ref", "qry" };
+    std::vector<std::string> seqs(2);
+    const std::string bases = "ACGT";
+
+    for(size_t i = 0; i < bases.size(); i++) {
+        for(size_t j = 0; j < bases.size(); j++) {
+            int count = static_cast<int>(sequence.substitutions(i, j));
+            for(int k = 0; k < count; k++) {
+                seqs[0] += bases[i];
+                seqs[1] += bases[j];
+            }
+        }
+    }
+
+    bpp::VectorSiteContainer result(&DNA);
+    for(int i = 0; i < 2; i++)
+        result.addSequence(bpp::BasicSequence(names[i], seqs[i], &DNA));
+
+    return result;
+}
+
+void checkAgainstBpp(const std::vector<Sequence>& sequences,
+                       bpp::SubstitutionModel& model,
+                       bpp::DiscreteDistribution& rates)
+{
+    using namespace bpp;
+    ASSERT_EQ(1, sequences.size());
+    const double starLL = star_optim::starLikelihood(model, rates, sequences);
+
+    VectorSiteContainer sites = createSites(sequences[0]);
+
+    Node *root = new Node(0),
+         *c1 = new Node(1, sites.getSequence(0).getName()),
+         *c2 = new Node(2, sites.getSequence(1).getName());
+    root->addSon(c1);
+    root->addSon(c2);
+    c1->setDistanceToFather(sequences[0].distance / 2);
+    c2->setDistanceToFather(sequences[0].distance / 2);
+    TreeTemplate<Node> tree(root);
+
+    DRHomogeneousTreeLikelihood calc(tree, sites, &model, &rates, true, false);
+    calc.initialize();
+    calc.computeTreeLikelihood();
+
+    const double bppLL = calc.getLogLikelihood();
+    EXPECT_NEAR(bppLL, starLL, 1e-3) << "Likelihood calculations do not match.";
+}
 
 TEST(GTR, simple_jc) {
     bpp::DNA dna;
@@ -26,6 +80,8 @@ TEST(GTR, simple_jc) {
     m.diagonal() = Eigen::Vector4d::Constant(1.0);
 
     s.distance = 0.02;
+
+    checkAgainstBpp(v, model, rates);
 
     const double ll = star_optim::starLikelihood(model, rates, v);
     const double expll = -5.62490959465585; // from bppml
@@ -47,9 +103,7 @@ TEST(GTR, known_distance) {
         1, 3, 2, 94;
     s.distance = 0.02;
 
-    const double expected = -702.603126357669; // From bppml
-    const double actual = star_optim::starLikelihood(model, rates, v);
-    ASSERT_NEAR(expected, actual, 1e-5);
+    checkAgainstBpp(v, model, rates);
 }
 
 
@@ -71,8 +125,5 @@ TEST(GTR, gamma_variation) {
         1, 3, 2, 94;
     s.distance = 0.02;
 
-    // bppml input.sequence.file=test.fasta input.tree.file=test.tre output.tree.file=/dev/null model=GTR(a=0.5, theta=0.6) optimization=None rate_distribution=Gamma(n=4, alpha=1.2)
-    const double expected = -714.208570235786; // From bppml
-    const double actual = star_optim::starLikelihood(model, rates, v);
-    ASSERT_NEAR(expected, actual, 1e-5);
+    checkAgainstBpp(v, model, rates);
 }
