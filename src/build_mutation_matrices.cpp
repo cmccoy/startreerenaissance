25802178f3fc28b0faba015796736c2266a5ec96
Connor McCoy <cmccoy@fhcrc.org>
2013-11-26 08:30:47 -0800

    CamelCase

--- src/build_mutation_matrices.cpp
+++ src/build_mutation_matrices.cpp
@@ -29,7 +29,7 @@ bool endsWith(const std::string& s, const std::string& suffix)
     }
 }
 
-inline int nt16_to_idx(const int b)
+inline int nt16ToIdx(const int b)
 {
     switch(b) {
         case 1: return 0; // A
@@ -69,19 +69,19 @@ int main(int argc, char* argv[])
 {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-    std::string fa_path, bam_path, output_path;
+    std::string fastaPath, bamPath, outputPath;
 
     bool no_ambiguous = false;
-    size_t max_records = 0;
+    size_t maxRecords = 0;
 
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help,h", "Produce help message")
         ("no-ambiguous", po::bool_switch(&no_ambiguous), "Do not include ambiguous sites")
-        ("max-records,n", po::value<size_t>(&max_records), "Maximum number of records to parse")
-        ("input-fasta,f", po::value<std::string>(&fa_path)->required(), "Path to (indexed) FASTA file")
-        ("input-bam,i", po::value<std::string>(&bam_path)->required(), "Path to BAM")
-        ("output-file,o", po::value<std::string>(&output_path)->required(), "Path to output file");
+        ("max-records,n", po::value<size_t>(&maxRecords), "Maximum number of records to parse")
+        ("input-fasta,f", po::value<std::string>(&fastaPath)->required(), "Path to (indexed) FASTA file")
+        ("input-bam,i", po::value<std::string>(&bamPath)->required(), "Path to BAM")
+        ("output-file,o", po::value<std::string>(&outputPath)->required(), "Path to output file");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -93,41 +93,41 @@ int main(int argc, char* argv[])
 
     po::notify(vm);
 
-    faidx_t* fidx = fai_load(fa_path.c_str());
+    faidx_t* fidx = fai_load(fastaPath.c_str());
     assert(fidx != NULL && "Failed to load FASTA index");
 
-    SamFile in(bam_path);
+    SamFile in(bamPath);
 
     bam1_t* b = bam_init1();
     size_t processed = 0;
 
-    std::vector<std::string> target_bases(in.fp->header->n_targets);
-    std::vector<int> target_len(in.fp->header->n_targets);
+    std::vector<std::string> targetBases(in.fp->header->n_targets);
+    std::vector<int> targetLen(in.fp->header->n_targets);
 
-    std::fstream out(output_path, std::ios::out | std::ios::trunc | std::ios::binary);
-    protoio::OstreamOutputStream raw_out(&out);
-    protoio::GzipOutputStream zip_out(&raw_out);
-    protoio::ZeroCopyOutputStream* outptr = &raw_out;
-    if(endsWith(output_path, ".gz"))
-        outptr = &zip_out;
-    protoio::CodedOutputStream coded_out(outptr);
+    std::fstream out(outputPath, std::ios::out | std::ios::trunc | std::ios::binary);
+    protoio::OstreamOutputStream rawOut(&out);
+    protoio::GzipOutputStream zipOut(&rawOut);
+    protoio::ZeroCopyOutputStream* outptr = &rawOut;
+    if(endsWith(outputPath, ".gz"))
+        outptr = &zipOut;
+    protoio::CodedOutputStream codedOut(outptr);
 
     while(samread(in.fp, b) >= 0) {
-        if(max_records > 0 && processed > max_records)
+        if(maxRecords > 0 && processed > maxRecords)
             break;
         processed++;
         const char* target_name = in.fp->header->target_name[b->core.tid];
-        if(target_bases[b->core.tid].empty()) {
-            char* ref = fai_fetch(fidx, target_name, &target_len[b->core.tid]);
+        if(targetBases[b->core.tid].empty()) {
+            char* ref = fai_fetch(fidx, target_name, &targetLen[b->core.tid]);
             assert(ref != nullptr && "Missing reference");
-            target_bases[b->core.tid] = ref;
+            targetBases[b->core.tid] = ref;
             free(ref);
         }
 
         const uint32_t* cigar = bam1_cigar(b);
         const uint8_t* seq = bam1_seq(b);
         uint32_t qi = 0, ri = b->core.pos;
-        const std::string& ref = target_bases[b->core.tid];
+        const std::string& ref = targetBases[b->core.tid];
 
         std::vector<int> mutations(16, 0);
 
@@ -140,8 +140,8 @@ int main(int argc, char* argv[])
             const uint32_t consumes = bam_cigar_type(cigar[cidx]); // bit 1: consume query; bit 2: consume reference
             if((consumes & 0x3) == 0x3) {  // Reference and query
                 for(uint32_t i = 0; i < clen; i++) {
-                    const int qb = nt16_to_idx(bam1_seqi(seq, qi + i)),
-                              rb = nt16_to_idx(bam_nt16_table[static_cast<int>(ref[ri + i])]);
+                    const int qb = nt16ToIdx(bam1_seqi(seq, qi + i)),
+                              rb = nt16ToIdx(bam_nt16_table[static_cast<int>(ref[ri + i])]);
                     if(qb < 4 && rb < 4 && (!no_ambiguous || bq[qi + i] % 100 == 0)) {
                         mutations[(rb * 4) + qb] += 1;
                     }
@@ -158,8 +158,8 @@ int main(int argc, char* argv[])
         for(const int i : mutations)
             count.add_mutations(i);
 
-        coded_out.WriteVarint32(count.ByteSize());
-        count.SerializeWithCachedSizes(&coded_out);
+        codedOut.WriteVarint32(count.ByteSize());
+        count.SerializeWithCachedSizes(&codedOut);
     }
 
     bam_destroy1(b);
