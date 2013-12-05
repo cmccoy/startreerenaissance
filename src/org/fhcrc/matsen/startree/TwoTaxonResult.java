4e231247b1fd5d7b1c6cb432205fd94a22990c72
Connor McCoy <cmccoy@fhcrc.org>
2013-12-05 10:06:02 -0500

    implement add

--- src/org/fhcrc/matsen/startree/TwoTaxonResult.java
+++ src/org/fhcrc/matsen/startree/TwoTaxonResult.java
@@ -1,6 +1,8 @@
 package org.fhcrc.matsen.startree;
 
 import cern.colt.matrix.DoubleMatrix2D;
+import cern.jet.math.Functions;
+import com.google.common.base.Preconditions;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,14 +16,33 @@ public class TwoTaxonResult {
             unconditionalNonsynonymous, unconditionalSynonymous;
 
     public TwoTaxonResult(DoubleMatrix2D conditionalNonsynonymous, DoubleMatrix2D conditionalSynonymous, DoubleMatrix2D unconditionalNonsynonymous, DoubleMatrix2D unconditionalSynonymous) {
+        Preconditions.checkArgument(conditionalNonsynonymous.rows() == conditionalSynonymous.rows());
+        Preconditions.checkArgument(conditionalNonsynonymous.rows() == unconditionalNonsynonymous.rows());
+        Preconditions.checkArgument(conditionalNonsynonymous.rows() == unconditionalSynonymous.rows());
+        Preconditions.checkArgument(conditionalNonsynonymous.columns() == conditionalSynonymous.columns());
+        Preconditions.checkArgument(conditionalNonsynonymous.columns() == unconditionalNonsynonymous.columns());
+        Preconditions.checkArgument(conditionalNonsynonymous.columns() == unconditionalSynonymous.columns());
+
         this.conditionalNonsynonymous = conditionalNonsynonymous;
         this.conditionalSynonymous = conditionalSynonymous;
         this.unconditionalNonsynonymous = unconditionalNonsynonymous;
         this.unconditionalSynonymous = unconditionalSynonymous;
     }
 
-    public TwoTaxonResult add (final TwoTaxonResult other) {
+    public TwoTaxonResult plus(final TwoTaxonResult other) {
+        Preconditions.checkArgument(other.conditionalNonsynonymous.rows() == conditionalNonsynonymous.rows());
+        Preconditions.checkArgument(other.conditionalNonsynonymous.columns() == conditionalNonsynonymous.columns());
 
+        DoubleMatrix2D cn = conditionalNonsynonymous.copy(),
+                cs = conditionalSynonymous.copy(),
+                un = unconditionalNonsynonymous.copy(),
+                us = unconditionalNonsynonymous.copy();
+        cn.assign(other.conditionalNonsynonymous, Functions.plus);
+        cs.assign(other.conditionalSynonymous, Functions.plus);
+        un.assign(other.unconditionalNonsynonymous, Functions.plus);
+        us.assign(other.unconditionalSynonymous, Functions.plus);
+
+        return new TwoTaxonResult(cn, cs, un, us);
     }
 
     public DoubleMatrix2D getUnconditionalSynonymous() {
