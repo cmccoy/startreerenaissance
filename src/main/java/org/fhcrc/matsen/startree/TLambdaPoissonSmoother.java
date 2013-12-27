2bf16adcc23f13c4e6fdbdbc261c76e5624a364a
Connor McCoy <cmccoy@fhcrc.org>
2013-12-27 14:56:04 -0800

    Fix bounds, passing test

--- src/main/java/org/fhcrc/matsen/startree/TLambdaPoissonSmoother.java
+++ src/main/java/org/fhcrc/matsen/startree/TLambdaPoissonSmoother.java
@@ -1,5 +1,6 @@
 package org.fhcrc.matsen.startree;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import org.apache.commons.math.special.Gamma;
 import org.apache.commons.math3.analysis.MultivariateFunction;
@@ -16,20 +17,21 @@ import java.util.logging.Logger;
 
 /**
  * Alternative version of @link dr.math.EmpiricalBayesPoissonSmoother, where coverage across positions is presumed to vary.
- *
+ * <p/>
  * we assume that <c>C_\ell \sim \mathrm{Poisson}(\lambda_\ell t_\ell)</c>,
  * where <c>t_\ell = \sum_i t_{\ell i}</c>, the sum of the branch lengths of sequences with a codon present at position <c>\ell</c>.
- *
+ * <p/>
  * Created by cmccoy on 12/27/13.
  */
 public class TLambdaPoissonSmoother {
 
     private static final Logger logger = Logger.getLogger(TLambdaPoissonSmoother.class.getName());
 
-    private final static int ADDL_INTERPOLATION_PTS = 1;
+    private final static int ADDL_INTERPOLATION_PTS = 2;
 
     /**
      * Smooth the counts <c>c</c>,
+     *
      * @param c
      * @param t
      * @return
@@ -45,13 +47,13 @@ public class TLambdaPoissonSmoother {
         PointValuePair result = estimateAlphaBeta(c, t);
         final double alpha = result.getPoint()[0],
                 beta = result.getPoint()[1];
-        logger.log(Level.INFO, "ML result: alpha={0} beta={1} ll={2}",
-                new Object[]{ alpha, beta, result.getValue()});
+        logger.log(Level.INFO, "MLE: alpha={0} beta={1} logL={2}",
+                new Object[]{alpha, beta, result.getValue()});
 
         final double[] smoothed = new double[c.length];
 
         // TODO: support random draws
-        for(int i = 0; i < c.length; i++) {
+        for (int i = 0; i < c.length; i++) {
             // posterior mean = alpha /
             smoothed[i] = (c[i] + alpha) / (t[i] + beta);
         }
@@ -59,7 +61,15 @@ public class TLambdaPoissonSmoother {
         return smoothed;
     }
 
-    private static PointValuePair estimateAlphaBeta(final double[] c, final double[] t) {
+    /**
+     * Estimate alpha, beta given c, t
+     *
+     * @param c
+     * @param t
+     * @return
+     */
+    @VisibleForTesting
+    static PointValuePair estimateAlphaBeta(final double[] c, final double[] t) {
         // Optimize. point[0] = alpha, point[1] = beta
         final int dim = 2;
         BOBYQAOptimizer optimizer = new BOBYQAOptimizer(2 * dim + ADDL_INTERPOLATION_PTS);
@@ -73,30 +83,33 @@ public class TLambdaPoissonSmoother {
             }
         };
 
-        final double lowerBounds[] = new double[] {0.0, 0.0};
-        final double upperBounds[] = new double[] {5000, 5000};
+        // Bounds - both must be positive
+        final double lowerBounds[] = new double[]{1e-8, 1e-8};
+        final double upperBounds[] = new double[]{Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY};
+        final double initial[] = new double[]{1.0, 1.0};
         PointValuePair result = optimizer.optimize(
                 new MaxEval(1000),
+                new InitialGuess(initial),
                 GoalType.MAXIMIZE,
                 new ObjectiveFunction(fn),
-                new SimpleBounds(lowerBounds, upperBounds),
-                new InitialGuess(new double[]{1, 1}));
+                new SimpleBounds(lowerBounds, upperBounds));
         return result;
     }
 
     /**
      * Returns the log likelihood of (c | t, alpha, beta)
-     *
+     * <p/>
      * <c>\mathcal L = \left(\frac{\beta^\alpha}{\Gamma(\alpha)}\right)^L \prod_\ell \frac{t_\ell^{C_\ell}}{\Gamma(C_\ell+1)} \frac{\Gamma(C_\ell + \alpha)}{(t_\ell + \beta)^{C_\ell + \alpha}}</c>
      *
      * @param alpha Alpha parameter of gamma distribution
-     * @param beta Beta parameter of gamma distribution
-     * @param c Counts
-     * @param t Branch lengths
+     * @param beta  Beta parameter of gamma distribution
+     * @param c     Counts
+     * @param t     Branch lengths
      * @return log-likelihood
      */
-    private static double logLikelihood(final double alpha, final double beta,
-                                        final double[] c, final double[] t) {
+    @VisibleForTesting
+    static double logLikelihood(final double alpha, final double beta,
+                                final double[] c, final double[] t) {
         Preconditions.checkNotNull(c);
         Preconditions.checkNotNull(t);
         Preconditions.checkArgument(c.length == t.length,
@@ -110,7 +123,7 @@ public class TLambdaPoissonSmoother {
         result = 0;
 
         // Over sites
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             final double ci = c[i], ti = t[i];
             // Per Wolfram Alpha,
             // log(b^a×(t^c/(Gamma(c+1)))/(Gamma(a))×(Gamma(c+a))/(t+b)^(c+a)) simplifies to
@@ -124,7 +137,9 @@ public class TLambdaPoissonSmoother {
             result += x;
         }
 
-        //logger.log(Level.FINE, "LL = {0}", result);
+        logger.log(Level.FINE, "alpha={0} beta={1} LL={2}",
+                new Object[]{alpha, beta, result});
+
         return result;
     }
 }
