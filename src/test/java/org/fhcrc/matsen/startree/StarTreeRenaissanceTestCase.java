9e73c99c579e4336756b12db7f42c5412646d765
Connor McCoy <cmccoy@fhcrc.org>
2014-01-07 05:35:38 -0800

    more realistic timing

--- src/test/java/org/fhcrc/matsen/startree/StarTreeRenaissanceTestCase.java
+++ src/test/java/org/fhcrc/matsen/startree/StarTreeRenaissanceTestCase.java
@@ -35,7 +35,8 @@ public class StarTreeRenaissanceTestCase {
         alignment.addSequence(new Sequence(new Taxon("ref"), "CAGGTTCAGCTGGTGCAGTCTGGAGCTGAGGTGAAGAAGCCTGGGGCCTCAGTGAAGGTCTCCTGCAAGGCTTCTGGTTACACCTTTACCAGCTATGGTATCAGCTGGGTGCGACAGGCCCCTGGACAAGGGCTTGAGTGGATGGGATGGATCAGCGCTTACAATGGTAACACAAACTATGCACAGAAGCTCCAGGGCAGAGTCACCATGACCACAGACACATCCACGAGCACAGCCTACATGGAGCTGAGGAGCCTGAGATCTGACGACACGGCCGTGTATTACTGTGCGAGA"));
         alignment.addSequence(new Sequence(new Taxon("qry"), "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------NNCAGGGCAGAGTCACCATGACCACAGACACATCCACGAGCACAGCCCACCTGGAACTGAAGAGCCTGAGATCTGACGACACGGCCGTGTATTTCTGTGCGCGA"));
 
-        TwoTaxonResult r = StarTreeRenaissance.calculate(alignment, hkys, rates, 5000, 1000);
+        //TwoTaxonResult r = StarTreeRenaissance.calculate(alignment, hkys, rates, 20000, 20);
+        TwoTaxonResult r = StarTreeRenaissance.calculate(alignment, hkys, rates);
         assertEquals(r.getUnconditionalNonsynonymous().getColumnDimension(), alignment.getPatternCount() / 3);
         //System.err.format("uN:\n%s\nuS:\n%s\ncN:\n%s\ncS:\n%s\n",
                 //r.getUnconditionalNonsynonymous(),
