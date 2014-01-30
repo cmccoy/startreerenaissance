123183656aa067f2352df87969631f5dafefbc72
Connor McCoy <cmccoy@fhcrc.org>
2014-01-30 09:03:17 -0800

    Rename, reformat

--- src/main/scala/org/fhcrc/matsen/startree/spark/StarTreeSpark.scala
+++ src/main/scala/org/fhcrc/matsen/startree/spark/StarTreeSpark.scala
@@ -1,6 +1,7 @@
 package org.fhcrc.matsen.startree.spark
 
 // Start
+
 import java.io._
 import java.util.logging._
 import scala.collection.JavaConverters._
@@ -11,51 +12,73 @@ import org.apache.spark.SparkContext._
 import net.sf.samtools.SAMFileReader
 
 import org.fhcrc.matsen.startree._
-import org.fhcrc.matsen.startree.gson._
 
 import com.google.common.base.Preconditions
-import com.google.gson.GsonBuilder
-import com.google.gson.reflect.TypeToken
 import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.model.{ObjectMetadata, PutObjectRequest}
 
-case class Config(parallelism: Int = 12,
-                  prefix: String = "",
-                  smooth: Boolean = true,
-                  sample: Boolean = false,
-                  masterPath: String = "",
-                  sparkHome: String = "/root/spark",
-                  jarPath: String = "/root/startreerenaissance/target/scala-2.9.3/startreerenaissance-assembly-0.1.jar",
-                  consolidateFiles: Boolean = true,
-                  executorMemory: String = "8g",
-                  bucket: Option[String] = None,
-                  jsonPath: File = new File("."),
-                  fastaPath: File = new File("."),
-                  bamPath: File = new File("."))
+case class StarTreeSparkConfig(parallelism: Int = 12,
+                               prefix: String = "",
+                               smooth: Boolean = true,
+                               sample: Boolean = false,
+                               masterPath: String = "",
+                               sparkHome: String = "/root/spark",
+                               jarPath: String = "/root/startreerenaissance/target/scala-2.9.3/startreerenaissance-assembly-0.1.jar",
+                               consolidateFiles: Boolean = true,
+                               executorMemory: String = "8g",
+                               bucket: Option[String] = None,
+                               jsonPath: File = new File("."),
+                               fastaPath: File = new File("."),
+                               bamPath: File = new File("."))
 
 object StarTreeSpark {
   val appName = "StarTreeSpark"
   val logger = Logger.getLogger("org.fhcrc.matsen.startree.spark")
 
-  val parser = new scopt.OptionParser[Config](appName) {
+  val parser = new scopt.OptionParser[StarTreeSparkConfig](appName) {
     head(appName, "0.1")
-    opt[Int]('j', "parallelism") action { (x, c) => c.copy(parallelism = x) } text("Parallelism level")
-    opt[String]('p', "prefix") action { (x, c) => c.copy(prefix = x) } text("Prefix to add to each output file")
-    opt[Unit]('n', "no-smooth") action { (_, c) => c.copy(smooth = false) } text("Do *not* smooth parameter estimates using empirical bayes")
-    opt[Unit]("no-consolidate-files") action { (_, c) => c.copy(consolidateFiles = false) } text("Do *not* consolidate files")
-    opt[Unit]('n', "sample") action { (_, c) => c.copy(sample = true) } text("Sample rates from poisson-gamma, rather than just using the mean")
-    opt[String]('b', "bucket") action { (x, c) => c.copy(bucket=Some(x)) } text("Upload to bucket")
-    opt[String]('s', "spark-home") action { (x, c) => c.copy(sparkHome = x) } text("Spark home")
-    opt[String]('j', "jar-path") action { (x, c) => c.copy(jarPath = x) } text("Path to the application JAR")
-    opt[String]("executor-memory") action { (x, c) => c.copy(executorMemory = x) } text("Executor memory")
-    arg[String]("<master_path>") required() action { (x, c) => c.copy(masterPath = x) } text("path to SPARK master")
-    arg[File]("<json>") required() action { (x, c) => c.copy(jsonPath = x) } text("path to JSON model specification")
-    arg[File]("<fasta>") required() action { (x, c) => c.copy(fastaPath = x) } text("path to reference FASTA file")
-    arg[File]("<bam>") required() action { (x, c) => c.copy(bamPath = x) } text("path to BAM file with aligned reads.")
-    help("help") text("prints this usage text")
+    opt[Int]('j', "parallelism") action {
+      (x, c) => c.copy(parallelism = x)
+    } text ("Parallelism level")
+    opt[String]('p', "prefix") action {
+      (x, c) => c.copy(prefix = x)
+    } text ("Prefix to add to each output file")
+    opt[Unit]('n', "no-smooth") action {
+      (_, c) => c.copy(smooth = false)
+    } text ("Do *not* smooth parameter estimates using empirical bayes")
+    opt[Unit]("no-consolidate-files") action {
+      (_, c) => c.copy(consolidateFiles = false)
+    } text ("Do *not* consolidate files")
+    opt[Unit]('n', "sample") action {
+      (_, c) => c.copy(sample = true)
+    } text ("Sample rates from poisson-gamma, rather than just using the mean")
+    opt[String]('b', "bucket") action {
+      (x, c) => c.copy(bucket = Some(x))
+    } text ("Upload to bucket")
+    opt[String]('s', "spark-home") action {
+      (x, c) => c.copy(sparkHome = x)
+    } text ("Spark home")
+    opt[String]('j', "jar-path") action {
+      (x, c) => c.copy(jarPath = x)
+    } text ("Path to the application JAR")
+    opt[String]("executor-memory") action {
+      (x, c) => c.copy(executorMemory = x)
+    } text ("Executor memory")
+    arg[String]("<master_path>") required() action {
+      (x, c) => c.copy(masterPath = x)
+    } text ("path to SPARK master")
+    arg[File]("<json>") required() action {
+      (x, c) => c.copy(jsonPath = x)
+    } text ("path to JSON model specification")
+    arg[File]("<fasta>") required() action {
+      (x, c) => c.copy(fastaPath = x)
+    } text ("path to reference FASTA file")
+    arg[File]("<bam>") required() action {
+      (x, c) => c.copy(bamPath = x)
+    } text ("path to BAM file with aligned reads.")
+    help("help") text ("prints this usage text")
   }
 
-  def run(config: Config) {
+  def run(config: StarTreeSparkConfig) {
     val reader = new SAMFileReader(config.bamPath)
     System.err.format("Loading JSON from %s\n", config.jsonPath);
     val jsonReader = new BufferedReader(new FileReader(config.jsonPath));
@@ -75,8 +98,8 @@ object StarTreeSpark {
         new SparkContext(mp, appName)
       case mp =>
         new SparkContext(mp, appName,
-                         config.sparkHome,
-                         Seq(config.jarPath))
+          config.sparkHome,
+          Seq(config.jarPath))
     }
 
     val references = SAMUtils.readAllFasta(config.fastaPath)
@@ -89,15 +112,15 @@ object StarTreeSpark {
       }).toList, config.parallelism).keyBy(_.getTaxon(0).getId)
 
     val result = alignments.mapValues(a => {
-        val model = modelRates.map(hr => hr.getModel).asJava
-        val rates = modelRates.map(hr => hr.getSiteRateModel).asJava
-        StarTreeRenaissance.calculate(a, model, rates)
-      }).reduceByKey(_.plus(_), config.parallelism)
+      val model = modelRates.map(hr => hr.getModel).asJava
+      val rates = modelRates.map(hr => hr.getSiteRateModel).asJava
+      StarTreeRenaissance.calculate(a, model, rates)
+    }).reduceByKey(_.plus(_), config.parallelism)
 
     val bcastConfig = sc.broadcast(config)
 
     // Save each target to S3 if a bucket is given
-    if(!config.bucket.isEmpty) {
+    if (!config.bucket.isEmpty) {
       println("Uploading to S3 bucket " + config.bucket.get)
       val cred = sc.broadcast(new com.amazonaws.auth.DefaultAWSCredentialsProviderChain().getCredentials)
 
@@ -135,32 +158,33 @@ object StarTreeSpark {
     sc.stop()
 
     resultLocal foreach {
-        case (refName, v) => {
-          val sanitized = refName.replaceAll("[*/]+", "_")
-          val outName = config.prefix + sanitized + ".log"
-          val writer = new PrintStream(new File(outName))
-          val result = new StarTreeRenaissanceResult(v, bcastConfig.value.sample, StarTreeRenaissance.CHAIN_LENGTH / 10)
-          if(config.smooth)
-            result.getSmoothed.print(writer, true)
-          else
-            v.print(writer, true)
-          writer.close()
+      case (refName, v) => {
+        val sanitized = refName.replaceAll("[*/]+", "_")
+        val outName = config.prefix + sanitized + ".log"
+        val writer = new PrintStream(new File(outName))
+        val result = new StarTreeRenaissanceResult(v, bcastConfig.value.sample, StarTreeRenaissance.CHAIN_LENGTH / 10)
+        if (config.smooth)
+          result.getSmoothed.print(writer, true)
+        else
+          v.print(writer, true)
+        writer.close()
 
-          val jsonName = config.prefix + sanitized + ".json.gz"
-          val jsonStream = new java.util.zip.GZIPOutputStream(new java.io.FileOutputStream(jsonName))
-          val jsonWriter = new PrintStream(jsonStream)
-          val gson = org.fhcrc.matsen.startree.gson.getGsonBuilder.create
-          gson.toJson(result, jsonWriter)
-          jsonWriter.close()
-        }
+        val jsonName = config.prefix + sanitized + ".json.gz"
+        val jsonStream = new java.util.zip.GZIPOutputStream(new java.io.FileOutputStream(jsonName))
+        val jsonWriter = new PrintStream(jsonStream)
+        val gson = org.fhcrc.matsen.startree.gson.getGsonBuilder.create
+        gson.toJson(result, jsonWriter)
+        jsonWriter.close()
       }
+    }
   }
 
   def main(args: Array[String]) {
     // spark path, json fasta, sam
-    parser.parse(args, Config()) map { config =>
+    parser.parse(args, StarTreeSparkConfig()) map {
+      config =>
       // do stuff
-      run(config)
+        run(config)
     } getOrElse {
       // arguments are bad, error message will have been displayed
       System.exit(1)
