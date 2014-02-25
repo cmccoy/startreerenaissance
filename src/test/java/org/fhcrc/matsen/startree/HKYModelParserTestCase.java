bbd5b0ead0f0db975ab7a7a5841ffbedf4a1bbf0
Connor McCoy <cmccoy@fhcrc.org>
2014-02-24 21:09:25 -0800

    Verify with most recent version

--- src/test/java/org/fhcrc/matsen/startree/HKYModelParserTestCase.java
+++ src/test/java/org/fhcrc/matsen/startree/HKYModelParserTestCase.java
@@ -18,7 +18,7 @@ public class HKYModelParserTestCase {
     Reader reader = null;
     @Before
     public void setUp() throws Exception {
-        reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("sample_model2.json")));
+        reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("sample_model3.json")));
     }
     @After
     public void tearDown() throws Exception {
@@ -32,8 +32,8 @@ public class HKYModelParserTestCase {
 
         assertEquals(3, result.size());
         assertEquals(4, result.get(0).getModel().getFrequencyModel().getFrequencyCount());
-        assertEquals(1.4436320149021, result.get(1).getModel().getKappa(), 1e-6);
-        assertEquals(0.3199744637909102, result.get(0).getModel().getFrequencyModel().getFrequency(0), 1e-6);
-        assertEquals(1.004932107403357, result.get(1).getRate(), 1e-6);
+        assertEquals(2.353655980531744, result.get(1).getModel().getKappa(), 1e-6);
+        assertEquals(0.3462646341639471, result.get(0).getModel().getFrequencyModel().getFrequency(0), 1e-6);
+        assertEquals(1.061030771043232, result.get(1).getRate(), 1e-6);
     }
 }
